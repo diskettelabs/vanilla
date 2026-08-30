8c1860ef39fcffede97309fc7322a8418154747a
mileswa1q22 <mileswolfallen4@hotmail.com>
2026-08-30 01:31:26 -0500

    idk

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,7 @@
 /**
  * Centralized error handling utilities
  * Provides user-friendly error messages with actionable guidance
+ * you need to do a rewrite. This code is extremely messy
  */
 
 class AppError extends Error {
